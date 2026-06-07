bb438530e35d80e457b4ef84cf0693526a3a484d
sreejithn7777 <142855800+sreejithn7777@users.noreply.github.com>
2026-06-06 23:20:52 -0400

    version 2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello World Version 1"
+    return "Hello World Version 2"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
