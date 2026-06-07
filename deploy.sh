bd1b7f53870e79c31e88038fef90d3f38635a822
sreejithn7777 <142855800+sreejithn7777@users.noreply.github.com>
2026-06-06 21:45:17 -0400

    build file

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+APP_DIR=/home/ec2-user/webservice
+
+if [ ! -d "$APP_DIR" ]; then
+    git clone https://github.com/sreejithn7777/webservice.git $APP_DIR
+fi
+
+cd $APP_DIR
+
+git pull
+
+pip3 install -r requirements.txt
+
+pkill -f app.py || true
+
+nohup python3 app.py > app.log 2>&1 &
